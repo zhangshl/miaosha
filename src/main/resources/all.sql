3fef5879c178e228faa47859fd984e7600a9d58a
zhangshaolong001 <zhangshaolong001@ke.com>
2021-01-16 12:56:58 +0800

    add sql file

---
+++ src/main/resources/all.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `sk_goods` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '商品ID',
+  `goods_name` varchar(30) DEFAULT NULL COMMENT '商品名称',
+  `goods_title` varchar(64) DEFAULT NULL COMMENT '商品标题',
+  `goods_img` varchar(64) DEFAULT NULL COMMENT '商品图片',
+  `goods_detail` longtext COMMENT '商品详情',
+  `goods_price` decimal(10,2) DEFAULT NULL,
+  `goods_stock` int(11) DEFAULT '0' COMMENT '商品库存，-1表示没有限制',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1000004 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `sk_order` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL,
+  `order_id` bigint(20) DEFAULT NULL,
+  `goods_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
